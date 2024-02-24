f508d1c249b829dbb5fa6138250d1d5f2f4d5646
Ashsifat1511 <sifatashrarul@gmail.com>
2024-02-24 11:57:58 +0600

    database update database fetch database display in ui others

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfolio.Data;
+using portfolio.Models;
+
+namespace portfolio.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db; 
+        public CategoryController(ApplicationDbContext db) {
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- Migrations/20240224052256_populateCategory.Designer.cs
+++ Migrations/20240224052256_populateCategory.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using portfolio.Data;
 
@@ -10,9 +11,11 @@ using portfolio.Data;
 namespace portfolio.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240224052256_populateCategory")]
+    partial class populateCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
